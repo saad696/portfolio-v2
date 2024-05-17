265e0aef350eb03676d404176e59a71d3557a049
saad696 <shksaad911@gmail.com>
2024-05-17 09:28:07 +0530

    reversed portfolio order

--- app/(root)/portfolio/page.tsx
+++ app/(root)/portfolio/page.tsx
@@ -39,7 +39,7 @@ const Page = () => {
                     My <span className='text-purple-500'>Works</span>
                 </h1>
                 <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6'>
-                    {projects.map((project) => (
+                    {projects.reverse().map((project) => (
                         <ProjectCard key={project.projectName} data={project} />
                     ))}
                 </div>
